153b6f357dc108d871d261b9f74af6d99693ac64
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-26 17:09:36 -0300

    feat: redone some header and footer styles for mobile version with responsiveness and added icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,32 +1,80 @@
 import React from 'react'
 import './Footer.css'
 
-const Footer = () => (
-    <footer className='footer'>
-        <section className='logo-endereco'>
-            <img src="logo.png" alt="logo do hospital" className='logo-img-footer' />
-            <section className='endereço'>
-                <address>
-                    Rua Doutor sla oq tanto faz, 123<br />
-                    Santos/SP<br />
-                    Cep 123456789
-                </address>
-                <br />
-                <p>Central de Atendimento 24h<br />
-                    +55 (13) 123456789</p>
-                <br />
-                <p>Ouvidoria</p>
-                <p>+55 (13) 123456789</p>
-            </section>
-        </section>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
-        <section className='Voluntariado'>
-            <a href="/"><h4>Seja Voluntario</h4></a>
-            <br />
-            <p>Cada gesto importa. Vamos juntos cuidar de quem precisa</p>
-            <button className='btnSaibaMais'>Saiba Mais</button>
-        </section>
-    </footer>
+const Footer = () => (
+
+    <>
+        <footer className='footer-desktop'>
+            <section className='endereco'>
+                <img src="logo.png" alt="logo do hospital" className='logo-img-footer' />
+                <div className='endereco-text'>
+                    <address>
+                        Rua Doutor sla oq tanto faz, 123<br />
+                        Santos/SP<br />
+                        Cep 123456789
+                    </address>
+                    <br />
+                    <p>Central de Atendimento 24h<br />
+                        +55 (13) 123456789</p>
+                    <br />
+                    <p>Ouvidoria</p>
+                    <p>+55 (13) 123456789</p>
+                </div>
+            </section>
+
+            <section className='Voluntariado'>
+                <a href="/"><h4>Seja Voluntario</h4></a>
+                <br />
+                <p>Cada gesto importa. Vamos juntos cuidar de quem precisa</p>
+                <button className='btnSaibaMais'>Saiba Mais</button>
+            </section>
+        </footer>
+
+
+
+        <footer className='footer-mobile'>
+            <section className='agendamento'>
+                <button className='btn-contact'>
+                    <a href="https://wa.me/5513981362813"><FontAwesomeIcon icon={faWhatsapp} style={{
+                        width: '15px'
+                        , height: '15px', color: '#fff'
+                    }} /> Agende por WhatsApp</a>
+                </button>
+            </section>
+
+            <div className='contact-mobile'>
+                <img src="logo.png" alt="logo-mobile" className='logo-mobile' />
+                <div className='divcentral'>
+                </div>
+                <FontAwesomeIcon icon={faPhoneAlt} /><a href="tel: 123456789" className='sla'><p>(13) 123456789</p></a>
+            </div>
+            <div className='social-icons'>
+                <a href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} style={{
+                    width: '25px'
+                    , height: '25px', color: '#fff'
+                }} /></a>
+                <a href="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebookF} style={{
+                    width: '20px'
+                    , height: '20px', color: '#fff'
+                }} /></a>
+                <a href="https://www.linkedin.com/"><FontAwesomeIcon icon={faLinkedinIn} style={{
+                    width: '25px'
+                    , height: '25px', color: '#fff'
+                }} /></a>
+            </div>
+
+        </footer>
+        <div className='copyright'>
+            <p>© 2025 Site Desenvolvido por Fellype Moura. Todos os direitos reservados.</p>
+        </div>
+    </>
 )
 
 export default Footer
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,37 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
-const Header = () => (
+const Header = () => {
+    const [menuAberto, setMenuAberto] = useState(false);
 
-    <>
-        <div className='top-bar'>
-            <ul className='top-bar-list'>
-                <li>13 912345678</li>
-                <li><a href="/">ouvidoria@medvida.org.br</a></li>
-                <li><a href="/">como chegar</a></li>
-            </ul>
-        </div>
-
-        <div className='header'>
-            <img src="logo.png" alt="logo do hospital" className='logo-img-header' />
-
-            <nav className='nav-menu'>
-                <ul className='menu-list'>
-                    <li className='dropdown'>
-                        <a href="/">Consultas e exames</a>
-                        <ul className='dropdown-menu'>
-                            <li><a href="/">teste</a></li>
-                            <li><a href="/">teste</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="/">Projeto mãos que Ajudam</a></li>
-                    <li><a href="/">Quem somos nós</a></li>
-                    <li><a href="/">Login</a></li>
-                    <li><a href="/">Cadastro</a></li>
+    return (
+        <>
+            <div className='top-bar'>
+                <ul className='top-bar-list'>
+                    <li>13 912345678</li>
+                    <li><a href="/">ouvidoria@medvida.org.br</a></li>
+                    <li><a href="/">como chegar</a></li>
                 </ul>
-            </nav>
-        </div>
-    </>
-)
+            </div>
+
+            <div className='header'>
+                <img src="logo.png" alt="logo do hospital" className='logo-img-header' />
+
+                <nav className='nav-menu-desktop'>
+                    <ul className='menu-list'>
+                        <li className='dropdown'>
+                            <a href="/">Consultas e exames</a>
+                            <ul className='dropdown-menu'>
+                                <li><a href="/">teste</a></li>
+                                <li><a href="/">teste</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="/">Projeto mãos que Ajudam</a></li>
+                        <li><a href="/">Quem somos nós</a></li>
+                        <li><a href="/">Login</a></li>
+                        <li><a href="/">Cadastro</a></li>
+                    </ul>
+                </nav>
+
+                <div className="menu-toggle" onClick={() => setMenuAberto(!menuAberto)}>
+                    {menuAberto ? '✖' : '☰'} 
+                </div>
+                </div>
+
+                <div className={`side-menu ${menuAberto ? 'aberto' : ''}`}>
+                    <ul className='menu-list'>
+                        <li className='dropdown'>
+                            <a href="/">Consultas e exames</a>
+                            <ul className='dropdown-menu'>
+                                <li><a href="/">teste</a></li>
+                                <li><a href="/">teste</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="/">Projeto mãos que Ajudam</a></li>
+                        <li><a href="/">Quem somos nós</a></li>
+                        <li><a href="/">Login</a></li>
+                        <li><a href="/">Cadastro</a></li>
+                    </ul>
+
+            </div>
+        </>
+    )
+}
 
 export default Header
